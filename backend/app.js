5335f2b9144f794a48df6675c63244af478ffe0e
Dhruv <dhruvsinghal1973@email.com>
2024-04-17 17:06:01 +0530

    env

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const connectToDb = require('./config/db.confg.js');
-const userRouter = require('./route/userRoute.js');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./config/db.confg.js");
+const userRouter = require("./route/userRoute.js");
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: [process.env.CLIENT_URL], credentials: true}))
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(cookieParser());
 
-app.use('/', userRouter);
+app.use("/", userRouter);
 
 // db init
 connectToDb();
