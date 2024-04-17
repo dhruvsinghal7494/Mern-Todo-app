9517d47282757b3e3ad159981a2981db135fbe10
Dhruv <dhruvsinghal1973@email.com>
2024-04-17 17:18:53 +0530

    URL

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,9 @@ const userRouter = require("./route/userRoute.js");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({ origin: ["https://mern-todo-app-end.vercel.app"], credentials: true })
+);
 app.use(cookieParser());
 
 app.use("/", userRouter);
